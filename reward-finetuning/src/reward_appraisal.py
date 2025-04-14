df162ad86b9b86c11e8a0ebf905371d105cc48d8
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-14 09:17:35 +0100

    update

--- reward-finetuning/src/reward_appraisal.py
+++ reward-finetuning/src/reward_appraisal.py
@@ -1,11 +1,16 @@
-import json
+import json, yaml
 import numpy as np
+import pandas as pd
 from sklearn.cluster import KMeans
 
 
 def clustering():
 
     envent_data = json.load(open('../../data/reward-appraisal/envent/envent_organized.json'))
+    emotion_to_sentiment = yaml.load(
+        open('../data/emotion_to_sentiment.yaml'), 
+        Loader=yaml.FullLoader
+    )
 
     # record appraisal profiles
     appraisal_mtx = np.zeros((len(envent_data), 21))
@@ -15,8 +20,25 @@ def clustering():
         appraisal_mtx[idx] = cur_appraisal_profile
         emotion_labels.append(val['emotion_label'])
 
-    kmeans = KMeans(n_clusters=2, random_state=0, )
-    print(appraisal_mtx)
+    sentiment_labels = [
+        emotion_to_sentiment[label] for label in emotion_labels
+    ]
+
+    kmeans = KMeans(
+        n_clusters=2, 
+        random_state=0, 
+    )
+
+    kmeans.fit(appraisal_mtx)
+    labels = kmeans.labels_
+
+    organized_results = {
+        'cluster': labels,
+        'sentiment': sentiment_labels,
+    }
+
+    out_df = pd.DataFrame(organized_results)
+    out_df.to_csv('../../exp_data/cluster_result.csv', index=False)
 
 
 if __name__ == '__main__':
