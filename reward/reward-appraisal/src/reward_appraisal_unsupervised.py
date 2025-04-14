e06f25e40693909eeb2a2c0652a4b9a0f8172751
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-14 16:01:47 +0100

    update

--- reward/reward-appraisal/src/reward_appraisal_unsupervised.py
+++ reward/reward-appraisal/src/reward_appraisal_unsupervised.py
@@ -8,11 +8,74 @@ from copy import deepcopy
 import plotly.express as px
 import matplotlib.pyplot as plt
 
+import torch
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.metrics import accuracy_score, f1_score
 
 from reward_appraisal_predict import AppraisalPredictor
+
+
+def down_project_data(appraisal_mtx: np.ndarray):
+
+    appraisal_mtx_embedded = TSNE(
+        n_components=2, 
+        learning_rate='auto',
+        init='random', 
+        perplexity=20,
+        random_state=12,
+    ).fit_transform(appraisal_mtx)
+
+    return appraisal_mtx_embedded
+
+
+def clustering(appraisal_mtx: np.ndarray):
+    kmeans = KMeans(
+        n_clusters=2, 
+        random_state=12, 
+    )
+
+    kmeans.fit(appraisal_mtx)
+    return kmeans.labels_
+
+
+def evaluate(labels: list, sentiment_labels: list) -> tuple:
+    result_save_fpath = '../../../exp_data/appraisal_clustering_result.json'
+    if not os.path.exists(result_save_fpath):
+        organized_results = {
+            'cluster': labels,
+            'sentiment': sentiment_labels,
+        }
+
+        out_df = pd.DataFrame(organized_results)
+        out_df.to_csv(result_save_fpath, index=False)
+
+    pred_labels = deepcopy(labels)
+    numerical_sentiment_labels = [
+        1 if label == 'negative' else 0 for label in sentiment_labels
+    ]
+
+    accuracy = accuracy_score(numerical_sentiment_labels, pred_labels)
+    f1 = f1_score(numerical_sentiment_labels, pred_labels)
+
+    return accuracy, f1
+
+
+def visualize_clustered_data(vis_data: pd.DataFrame):
+    # Create scatter plot with Plotly
+    fig = px.scatter(
+        vis_data,
+        x='x',
+        y='y',
+        color='sentiment',
+        title="Appraisal Embedded Scatter Plot",
+        labels={'x': 'Dimension 1', 'y': 'Dimension 2'},
+    )
+    fig.update_traces(marker=dict(size=8, opacity=0.75, line=dict(width=1, color='DarkSlateGrey')))
+    fig.update_layout(legend_title_text='Sentiment')
+    fig.write_image(f"../../../exp_data/appraisal_clustering.png")
+    fig.write_html(f"../../../exp_data/appraisal_clustering.html")
+    fig.show()
 
 
 def parse_args():
@@ -31,9 +94,12 @@ def parse_args():
     return parser.parse_args()
 
 
-def clustering():
+def main():
 
     args = parse_args()
+
+    if args.predict_appraisal:
+        assert torch.cuda.is_available(), "CUDA is not available. Appraisal prediction requires GPU."
 
     envent_data = json.load(open('../../../data/reward-appraisal/envent/envent_organized.json'))
     emotion_to_sentiment = yaml.load(
@@ -58,62 +124,48 @@ def clustering():
         emotion_to_sentiment[label] for label in emotion_labels
     ]
 
-    appraisal_embedded = TSNE(n_components=2, learning_rate='auto',
-                  init='random', perplexity=3).fit_transform(appraisal_mtx)
+    appraisal_mtx_embedded = down_project_data(
+        appraisal_mtx=appraisal_mtx,
+    )
 
     # make binary sentiment label for visualization
     binary_sentiment_labels = [
         'negative' if label == 'negative' else 'positive' for label in sentiment_labels
     ]
 
-    vis_data = pd.DataFrame({ 
-        'x': [point[0] for point in appraisal_embedded],
-        'y': [point[1] for point in appraisal_embedded],
-        'sentiment': binary_sentiment_labels,
-    })
-
-    kmeans = KMeans(
-        n_clusters=2, 
-        random_state=0, 
+    clustering_labels = clustering(
+        appraisal_mtx=appraisal_mtx,
+    )
+    embedded_clustering_labels = clustering(
+        appraisal_mtx=appraisal_mtx_embedded,
     )
 
-    kmeans.fit(appraisal_embedded)
-    labels = kmeans.labels_
+    acc, f1 = evaluate(
+        labels=clustering_labels,
+        sentiment_labels=sentiment_labels,
+    )
+    dp_acc, dp_f1 = evaluate(
+        labels=embedded_clustering_labels,
+        sentiment_labels=sentiment_labels,
+    )
 
-    result_save_fpath = '../../../exp_data/appraisal_clustering_result.json'
-    if not os.path.exists(result_save_fpath):
-        organized_results = {
-            'cluster': labels,
-            'sentiment': sentiment_labels,
-        }
+    print("\n\nEvaluation on Full Appraisal Profile (21d)")
+    print(f"accuracy: {acc}")
+    print(f"f1: {f1}") 
+    print("\nEvaluation on T-SNE Embedded Appraisal Profile (2d)")
+    print(f"accuracy: {dp_acc}")
+    print(f"f1: {dp_f1}") 
 
-        out_df = pd.DataFrame(organized_results)
-        out_df.to_csv(result_save_fpath, index=False)
-
-    pred_labels = deepcopy(labels)
-    numerical_sentiment_labels = [
-        1 if label == 'negative' else 0 for label in sentiment_labels
-    ]
-
-    print(f"accuracy: {accuracy_score(numerical_sentiment_labels, pred_labels)}")
-    print(f"f1: {f1_score(numerical_sentiment_labels, pred_labels)}") 
-
-    if not os.path.exists('../../../exp_data/appraisal_clustering.png'):
-        # Create scatter plot with Plotly
-        fig = px.scatter(
-            vis_data,
-            x='x',
-            y='y',
-            color='sentiment',
-            title="Appraisal Embedded Scatter Plot",
-            labels={'x': 'Dimension 1', 'y': 'Dimension 2'},
-        )
-        fig.update_traces(marker=dict(size=8, opacity=0.75, line=dict(width=1, color='DarkSlateGrey')))
-        fig.update_layout(legend_title_text='Sentiment')
-        fig.write_image(f"../../../exp_data/appraisal_clustering.png")
-        fig.write_html(f"../../../exp_data/appraisal_clustering.html")
-        fig.show()
+    # visualize the down-projected data
+    vis_data = pd.DataFrame({ 
+        'x': [point[0] for point in appraisal_mtx_embedded],
+        'y': [point[1] for point in appraisal_mtx_embedded],
+        'sentiment': binary_sentiment_labels,
+    })
+    visualize_clustered_data(
+        vis_data=vis_data
+    )
 
 
 if __name__ == '__main__':
-    clustering()
+    main()
