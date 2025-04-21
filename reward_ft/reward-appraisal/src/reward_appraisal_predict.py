34164c64ca7c34af61cda4f10a8038716a99d6d4
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-21 11:30:48 +0100

    reorganize reward funcs

---
+++ reward_ft/reward-appraisal/src/reward_appraisal_predict.py
@@ -0,0 +1,109 @@
+import json
+import asyncio
+import os, sys
+import numpy as np
+from copy import deepcopy
+from tqdm.asyncio import tqdm as atqdm
+
+import torch
+
+root_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../"))
+sys.path.append(root_dir)
+
+from src.utils.llm_inference import vLLMServer, OpenAIAsyncInference
+
+
+class AppraisalPredictor:
+
+    def __init__(
+        self, 
+        data: dict,
+        prompt_template: dict,
+        model_name: str,
+        model_path_dict: dict,
+    ) -> None:
+
+        self.data = data
+        self.prompt_template = prompt_template
+        self.model_name = model_name
+        self.model_path_dict = model_path_dict
+        
+        # laod model weight path and model quantization config
+        model_config = self.model_path_dict[self.model_name]
+        self.model_path = model_config['path']
+        self.quantization = model_config['quantization']
+
+
+    async def predict(self, envent_data: dict) -> tuple:
+
+        # initialize the vllm server
+        vllm_server = vLLMServer(
+            model_path=self.model_path,
+            world_size=torch.cuda.device_count(),
+            quantization=self.quantization,
+        )
+
+        openai_async_client = vllm_server.start_vllm_server()
+
+        try:
+            appraisal_desc_msg_list = []
+            candidate_emotion_labels = []
+            for entry_idx, (id, entry) in enumerate(self.data.items()):
+                cur_situation = entry['context'].strip()
+                cur_appraisal_prediction_prompt = deepcopy(self.prompt_template)
+                cur_appraisal_prediction_prompt[1]['content'] = cur_appraisal_prediction_prompt[1]['content'].replace(
+                    '{{scenario}}', cur_situation
+                )
+
+                appraisal_desc_msg_list.append(cur_appraisal_prediction_prompt)
+                candidate_emotion_labels.append(entry['emotion_label'])
+
+            semaphore = asyncio.Semaphore(100)
+            appraisal_pred_response_list = [openai_async_client.process_with_semaphore(
+                semaphore=semaphore,
+                model=self.model_name,
+                message=msg,
+                return_json=False,
+            ) for msg in appraisal_desc_msg_list]
+
+            appraisal_desc_list = await atqdm.gather(*appraisal_pred_response_list)
+
+            # parse the appraisal prediction response
+            appraisal_desc_list = [
+                ele.split('<ratings>')[1].strip() if '<ratings>' in ele else ele
+                for ele in appraisal_desc_list
+            ]
+            appraisal_desc_list = [
+                ele.split('</ratings>')[0].strip() if '</ratings>' in ele else ele
+                for ele in appraisal_desc_list
+            ]
+
+            valid_idx = 0
+            out_emotion_labels = []
+            appraisal_mtx = np.zeros((len(envent_data), 21))
+            for i, entry in enumerate(appraisal_desc_list):
+                cur_predicted_appraisal_dims = entry.split('\n')
+                cur_predicted_appraisal_dims = [
+                    ele.replace('-', '').split(':') for ele in cur_predicted_appraisal_dims if ele.strip()
+                ]
+
+                cur_predicted_appraisal_dims_list = []
+                for entry in cur_predicted_appraisal_dims:
+                    if len(entry) == 2:
+                        try:
+                            cur_predicted_appraisal_dims_list.append(int(entry[1].strip()))
+                        except:
+                            continue
+
+                if len(cur_predicted_appraisal_dims_list) == 21:
+                    appraisal_mtx[valid_idx] = cur_predicted_appraisal_dims_list
+                    out_emotion_labels.append(candidate_emotion_labels[i])
+                    valid_idx += 1
+
+        finally:
+            vllm_server.kill_server() 
+
+        appraisal_mtx = appraisal_mtx[:valid_idx]
+
+        return appraisal_mtx, out_emotion_labels
+
---
+++ reward_ft/reward-appraisal/src/reward_appraisal_unsupervised.py
@@ -0,0 +1,186 @@
+import os
+import asyncio
+import argparse
+import json, yaml
+import numpy as np
+import pandas as pd
+from copy import deepcopy
+
+import plotly.express as px
+import matplotlib.pyplot as plt
+
+import torch
+from sklearn.manifold import TSNE
+from sklearn.cluster import KMeans
+from sklearn.metrics import accuracy_score, f1_score
+
+from reward_appraisal_predict import AppraisalPredictor
+
+
+def down_project_data(appraisal_mtx: np.ndarray):
+
+    appraisal_mtx_embedded = TSNE(
+        n_components=2, 
+        learning_rate='auto',
+        init='random', 
+        perplexity=20,
+        random_state=12,
+    ).fit_transform(appraisal_mtx)
+
+    return appraisal_mtx_embedded
+
+
+def clustering(appraisal_mtx: np.ndarray):
+    kmeans = KMeans(
+        n_clusters=2, 
+        random_state=12, 
+    )
+
+    kmeans.fit(appraisal_mtx)
+    return kmeans.labels_
+
+
+def evaluate(labels: list, sentiment_labels: list) -> tuple:
+    result_save_fpath = '../../../exp_data/appraisal_clustering_result.json'
+    if not os.path.exists(result_save_fpath):
+        organized_results = {
+            'cluster': labels,
+            'sentiment': sentiment_labels,
+        }
+
+        out_df = pd.DataFrame(organized_results)
+        out_df.to_csv(result_save_fpath, index=False)
+
+    pred_labels = deepcopy(labels)
+    numerical_sentiment_labels = [
+        1 if label == 'negative' else 0 for label in sentiment_labels
+    ]
+
+    accuracy = accuracy_score(numerical_sentiment_labels, pred_labels)
+    f1 = f1_score(numerical_sentiment_labels, pred_labels)
+
+    return accuracy, f1
+
+
+def visualize_clustered_data(vis_data: pd.DataFrame):
+    # Create scatter plot with Plotly
+    fig = px.scatter(
+        vis_data,
+        x='x',
+        y='y',
+        color='sentiment',
+        title="Appraisal Embedded Scatter Plot",
+        labels={'x': 'Dimension 1', 'y': 'Dimension 2'},
+    )
+    fig.update_traces(marker=dict(size=8, opacity=0.75, line=dict(width=1, color='DarkSlateGrey')))
+    fig.update_layout(legend_title_text='Sentiment')
+    fig.write_image(f"../../../exp_data/appraisal_clustering.png")
+    fig.write_html(f"../../../exp_data/appraisal_clustering.html")
+    fig.show()
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Prepare COKE dataset for finetuning")
+    parser.add_argument(
+        '--model', 
+        type=str,
+        default='llama8',
+        help='Model name to be used for rating appraisals'
+    )
+    parser.add_argument(
+        '--predict_appraisal',
+        action='store_true',
+        help='Flag to indicate if appraisal prediction is needed, otherwise use human annotating'
+    )
+    return parser.parse_args()
+
+
+async def main():
+
+    args = parse_args()
+
+    if args.predict_appraisal:
+        assert torch.cuda.is_available(), "CUDA is not available. Appraisal prediction requires GPU."
+
+    envent_data = json.load(open('../../../data/reward-appraisal/envent/envent_organized.json'))
+    emotion_to_sentiment = yaml.load(
+        open('../../../src/configs/emotion_to_sentiment.yaml'), 
+        Loader=yaml.FullLoader
+    )
+
+    if args.predict_appraisal:
+        prompt_template = yaml.load(
+            open('../prompts/predict_appraisal.yaml', 'r'),
+            Loader=yaml.SafeLoader,
+        )
+        model_path_dict = yaml.load(
+            open('../../../src/configs/llm_configs.yaml', 'r'),
+            Loader=yaml.SafeLoader,
+        )
+        appraisal_predictor = AppraisalPredictor(
+            data=envent_data,
+            prompt_template=prompt_template,
+            model_name=args.model,
+            model_path_dict=model_path_dict,
+        )
+        appraisal_mtx, emotion_labels = await appraisal_predictor.predict(
+            envent_data=envent_data,
+        )
+    else:
+        appraisal_mtx = np.zeros((len(envent_data), 21))
+        emotion_labels = []
+        # retrieve human annotation from EnVent dataset
+        for idx, val in enumerate(envent_data.values()):
+            cur_appraisal_profile = list(val['appraisal_dims'].values())
+            appraisal_mtx[idx] = cur_appraisal_profile
+            emotion_labels.append(val['emotion_label'])
+
+    sentiment_labels = [
+        emotion_to_sentiment[label] for label in emotion_labels
+    ]
+
+    appraisal_mtx_embedded = down_project_data(
+        appraisal_mtx=appraisal_mtx,
+    )
+
+    # make binary sentiment label for visualization
+    binary_sentiment_labels = [
+        'negative' if label == 'negative' else 'positive' for label in sentiment_labels
+    ]
+
+    clustering_labels = clustering(
+        appraisal_mtx=appraisal_mtx,
+    )
+    embedded_clustering_labels = clustering(
+        appraisal_mtx=appraisal_mtx_embedded,
+    )
+
+    acc, f1 = evaluate(
+        labels=clustering_labels,
+        sentiment_labels=sentiment_labels,
+    )
+    dp_acc, dp_f1 = evaluate(
+        labels=embedded_clustering_labels,
+        sentiment_labels=sentiment_labels,
+    )
+
+    print("\n\nEvaluation on Full Appraisal Profile (21d)")
+    print(f"accuracy: {acc}")
+    print(f"f1: {f1}") 
+    print("\nEvaluation on T-SNE Embedded Appraisal Profile (2d)")
+    print(f"accuracy: {dp_acc}")
+    print(f"f1: {dp_f1}") 
+
+    # visualize the down-projected data
+    vis_data = pd.DataFrame({ 
+        'x': [point[0] for point in appraisal_mtx_embedded],
+        'y': [point[1] for point in appraisal_mtx_embedded],
+        'sentiment': binary_sentiment_labels,
+    })
+    visualize_clustered_data(
+        vis_data=vis_data
+    )
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
