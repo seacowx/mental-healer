755b77446682dea81c1290e1473b121e1cb38896
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-21 13:31:11 +0100

    udpate

--- src/run.py
+++ src/run.py
@@ -11,7 +11,18 @@ def main():
     data = pd.read_csv('../data/situations/situations.csv')
     data = data.to_dict(orient='records')
 
+    print(data0[0])
+    raise SystemExit()
+
     therapost_trainer = TherapistTrainer(data=data)
+
+    temp_input_list = [
+        {
+            'situation': ele['situation'], 
+            'thought': ele['thought'],
+        }
+        for ele in data[:10]
+    ]
 
 
 if __name__ == "__main__":
--- src/train_therapist.py
+++ src/train_therapist.py
@@ -1,3 +1,4 @@
+import yaml
 from rewards.sentiment import SentimentReward
 
 
@@ -6,4 +7,24 @@ class TherapistTrainer:
 
     def __init__(self, data: list) -> None:
         self.data = data
+        self.sentiment_prompt = yaml.load(
+            './prompts/sentiment.yaml',
+            Loader=yaml.FullLoader,
+        )['input']
         self.sentiment_reward = SentimentReward()
+
+
+    def __compute_sentiment_reward(self, input_list: list) -> list:
+
+        # make prompt
+        input_msg_list = [
+            [{'role': 'user', 'content': self.sentiment_prompt.format(**ele)}]
+            for ele in input_list
+        ]
+
+        print(input_msg_list)
+        raise SystemExit()
+
+        sentiment_list = self.sentiment_reward.get_sentiment(
+            input_list=input_msg_list
+        )
