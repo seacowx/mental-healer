05509df0804729574c51efe51905e7be3358a9d2
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-21 14:23:06 +0100

    udpate

--- src/run.py
+++ src/run.py
@@ -15,6 +15,21 @@ def main():
 
     temp_input_list = data[:10]
 
+
+    test_data = json.load('../reward_ft/reward-sentiment/sentiment_data/reward-sentiment_test.json')
+
+    input_msg_list = [
+            [{'role': 'user', 'content': ele['instruction'].strip()} for ele in test_data]
+    ]
+    label_list = [
+        ele['label'].split('<emotion>')[-1].split('</emotion>')[0].lower().strip() for ele in test_data
+    ]
+
+    print(input_msg_list[0])
+    print('\n\n')
+    print(label_list)
+    raise SystemExit()
+
     therapist_trainer._TherapistTrainer__compute_sentiment_reward(
         input_list=temp_input_list
     )
