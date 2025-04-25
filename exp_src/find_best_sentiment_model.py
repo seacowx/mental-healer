162b64868993728f2abcba6399ca7f1047e44314
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-25 22:00:14 +0100

    update

--- exp_src/find_best_sentiment_model.py
+++ exp_src/find_best_sentiment_model.py
@@ -25,6 +25,8 @@ def evaluate(
 
     # filter out predictions that are not in the label space
     label_space = list(set(ground_truth))
+    print(label_space)
+    raise SystemExit()
     valid_predicted = [ele for ele in predicted if ele in label_space]
     valid_ground_truth = [
         gt_ele for (gt_ele, pred_ele) in zip(ground_truth, predicted) if pred_ele in label_space
