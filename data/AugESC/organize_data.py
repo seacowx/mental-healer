39eb7dcf47df34bb5f3b4a583b3a5834012187f3
Seacow <46582003+seacowx@users.noreply.github.com>
2025-04-28 14:56:40 +0100

    update

--- data/AugESC/organize_data.py
+++ data/AugESC/organize_data.py
@@ -9,9 +9,15 @@ def main():
     data = [line.strip() for line in data if line.strip()]
     data = [ast.literal_eval(line) for line in data]
 
-    with open('./augesc.json', 'w') as f:
+    init_dialogue_data = [
+        ele[0] for ele in data
+    ]
+
+    with open('./augesc_full.json', 'w') as f:
         json.dump(data, f, indent=4)
 
+    with open('./augesc.json', 'w') as f:
+        json.dump(init_dialogue_data, f, indent=4)
 
 if __name__ == "__main__":
     main()
