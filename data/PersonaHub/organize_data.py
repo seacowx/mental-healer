d0646a4aa4e9bc903fc7a4fdde1f6e1fd6492296
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-01 21:57:32 +0100

    update

--- data/PersonaHub/organize_data.py
+++ data/PersonaHub/organize_data.py
@@ -10,11 +10,36 @@ import string
 import random
 from tqdm import tqdm
 
+import torch
+from jinja2 import Template
+from vllm import LLM, SamplingParams
+
 import nltk
 from nltk.stem import PorterStemmer
 from nltk.tokenize import word_tokenize
 nltk.download("punkt")
 nltk.download('punkt_tab')
+
+
+def make_prompt(event_desc: str) -> str:
+
+    template = Template(
+        "Your task is to determine whether the given persona profile is describing an individual person or "
+        "a group of people.\n\n<persona>\n{{ event_desc }}\n</persona>\n\n"
+        "If the persona profile is describing an individual person, respond with '<decision>Individual</decision>'.\n"
+        "If the persona profile is describing a group of people, respond with '<decision>Group</decision>'.\n"
+        "Do not include any other text in your response."
+    )
+
+    return template.render(event_desc=event_desc.capitalize())
+
+
+def parse_output(output):
+    output = output.outputs[0].text
+    if '<decision>' in output and '</decision>' in output:
+        decision = output.split('<decision>')[1].split('</decision>')[0].strip().lower()
+        return decision
+    return ''
 
 
 def generate_id():
@@ -68,7 +93,7 @@ def main():
     with open('../PersonaHub/persona.jsonl', 'r') as f:
         persona_data = [json.loads(line) for line in f]
 
-    out_dict = {}
+    temp_out_dict = {}
     for entry in tqdm(persona_data):
         persona = entry['persona']
 
@@ -80,9 +105,59 @@ def main():
 
         if keep_persona:
             cur_id = generate_id()
-            while cur_id in out_dict:
+            while cur_id in temp_out_dict:
                 cur_id = generate_id()
-            out_dict[cur_id] = persona
+            temp_out_dict[cur_id] = persona
+
+    # remove persona profiles that are not describing an individual
+    model_path = (
+        '/scratch/prj/charnu/seacow_hf_cache/models--Qwen--Qwen3-32B/'
+        'snapshots/ba1f828c09458ab0ae83d42eaacc2cf8720c7957'
+    )
+    WORLD_SIZE = torch.cuda.device_count()
+    vllm = LLM(
+        model=model_path, 
+        max_model_len=2048,
+        tensor_parallel_size=WORLD_SIZE,
+        gpu_memory_utilization=0.9,
+        enforce_eager=True,
+    )
+    sampling_params = SamplingParams(
+        temperature=0.6,
+        top_p=0.95,
+        top_k=20,
+        max_tokens=8192,
+        presence_penalty=1.,
+    )
+
+    prompt_list = [
+        make_prompt(ele) for ele in temp_out_dict.values()
+    ]
+    msg_list = [
+        [{'role': 'user', 'content': ele}] for ele in prompt_list
+    ]
+
+    output_list = vllm.chat(
+        messages=msg_list,
+        sampling_params=sampling_params,
+        use_tqdm=True,
+        chat_template_kwargs={
+            "enable_thinking": True,
+        },
+    )
+
+    output_list = [
+        parse_output(output)
+        for output in output_list
+    ]
+
+    out_dict = {}
+    for output_label, (key, val) in zip(output_list, temp_out_dict.items()):
+        if output_label.lower() == 'individual':
+            out_dict[key] = val
+
+    print(f"Before Filtering: {len(temp_out_dict)}")
+    print(f"After Filtering: {len(out_dict)}")
 
     with open('./persona.json', 'w') as f:
         json.dump(out_dict, f, indent=4)
