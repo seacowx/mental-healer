1073bc509760dd0f544b7213f158a4691c4557d2
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-05 23:53:41 +0100

    update

--- data/PersonaHub/structurize_persona.py
+++ data/PersonaHub/structurize_persona.py
@@ -55,7 +55,7 @@ async def main():
                 max_tokens=4096,
                 top_p=0.95,
                 frequency_penalty=0.0,
-                presence_penalty=1.0,) for msg in msg_list
+                presence_penalty=1.0,) for msg in msg_list[:100]
             ]
         output_list = await atqdm.gather(*output_list)
 
