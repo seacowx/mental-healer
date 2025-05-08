cd69c0c70119d1f8811e9fe0bafe1a81577bd4bf
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-08 15:07:27 +0100

    update

--- src/run.sh
+++ src/run.sh
@@ -8,6 +8,9 @@ CUDA_VISIBLE_DEVICES=0 trl vllm-serve \
     --tensor_parallel_size 1 \
     --gpu-memory-utilization 0.75
 
+printf "\n\n-----------------------------------------------------------------------"
+printf "\n\nvLLM server started. Starting training..."
+printf "-----------------------------------------------------------------------\n\n"
 
 CUDA_VISIBLE_DEVICES=1 python debug.py \
     --model_path $MODEL_PATH \
