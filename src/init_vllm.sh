1362bd795133935e7f5f2e6dcf8cc52656fd07d5
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-08 22:49:42 +0100

    update

--- src/init_vllm.sh
+++ src/init_vllm.sh
@@ -3,12 +3,22 @@ MODEL_PATH='Qwen/Qwen3-8B'
 TRL_VLLM_PORT=8880
 TENSOR_PARALLEL_SIZE=$1
 
+if [ -z "$TENSOR_PARALLEL_SIZE" ]; then
+    TENSOR_PARALLEL_SIZE=1
+fi
+
+# make cuda visible devices to be 0 and 1 if TENSOR_PARALLEL_SIZE is 2
+if [ $TENSOR_PARALLEL_SIZE -eq 2 ]; then
+    CUDA_VISIBLE_DEVICES=0,1
+else
+    CUDA_VISIBLE_DEVICES=0
+fi
 
 printf "\n\n=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n"
 printf "Initializing vLLM server...\n"
 printf "=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=\n\n"
 
-CUDA_VISIBLE_DEVICES=0 trl vllm-serve \
+CUDA_VISIBLE_DEVICES=$CUDA_VISIBLE_DEVICES trl vllm-serve \
     --model $MODEL_PATH \
     --port $TRL_VLLM_PORT \
     --tensor_parallel_size $TENSOR_PARALLEL_SIZE \
