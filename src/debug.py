d0fc843cfb2146b0454c46e1f8154294899995b9
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-12 14:21:55 +0100

    update

--- src/debug.py
+++ src/debug.py
@@ -1,9 +1,10 @@
-import json
+import json, yaml
 
 
 async def test_sentiment(sentiment_reward_model):
 
     test_data = json.load(open('../reward_ft/reward-sentiment/sentiment_data/reward-sentiment_test.json'))
+    sentiment_label_mapping = yaml.safe_load(open('./configs/emotion_to_sentiment.yaml'))
 
     input_msg_list = [
         [{'role': 'user', 'content': ele['instruction'].strip()}]
@@ -14,16 +15,6 @@ async def test_sentiment(sentiment_reward_model):
         for ele in test_data
     ]
 
-    eval_result_dict = {
-        'lora_idx': [],
-        'accuracy': [],
-        'f1': []
-    }
-    sentiment_eval_result_dict = {
-        'lora_idx': [],
-        'accuracy': [],
-        'f1': []
-    }
     outputs = await sentiment_reward_model.get_sentiment(
         input_msg_list
     )
