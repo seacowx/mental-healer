ecf9392c78f8a4e86b3e7bfe6e7a4e0a593c3431
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-14 00:23:40 +0100

    update

--- src/utils/custom_vllm.py
+++ src/utils/custom_vllm.py
@@ -192,9 +192,11 @@ class CustomLLM(LLM):
                 )
 
                 # modify the prompt to put the coping strategy content in between the <think> and </think> tags
-                prompt_instruction, coping_strategy_content = prompt_str.rsplit('<think>')
+                prompt_instruction, coping_strategy_content = prompt_str.rsplit('<think>', 1)
 
-                coping_strategy_content = coping_strategy_content.rsplit('</think>')[-1].rsplit('<|im_end|>')[0].strip()
+                coping_strategy_content = coping_strategy_content \
+                    .rsplit('</think>', 1)[-1] \
+                    .rsplit('<|im_end|>', 1)[0].strip()
                 coping_strategy_content += '\n\n' + self.coping_postfix
                 prompt_str = prompt_instruction.strip() + '\n<think>\n' + coping_strategy_content + '\n</think>'
 
