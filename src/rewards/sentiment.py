b27f5526726c3c4e3971904e7cba5afc59df8152
Seacow <46582003+seacowx@users.noreply.github.com>
2025-05-18 23:17:17 +0100

    update

--- src/rewards/sentiment.py
+++ src/rewards/sentiment.py
@@ -98,7 +98,8 @@ class SentimentReward:
             if output_msg_idx in filled_idx_list:
                 cur_output = self.__parse_output(outputs[output_idx])
 
-                sentiment = self.sentiment_mapping.get(cur_output, 'negative')
+                if cur_output not in ['positive', 'negative']:
+                    sentiment = self.sentiment_mapping[cur_output]
                 sample_idx, thought_idx = output_msg_idx                
 
                 output_list[sample_idx][thought_idx] = sentiment
